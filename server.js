c620d442a1c45f7eecce19288b62e852ba33605c
hydedall <hyde.dallas1@gmail.com>
2022-08-30 23:11:49 -0400

    rest of the roots

--- server.js
+++ server.js
@@ -10,6 +10,26 @@ app.get('/', (req, res)=>{
     res.send(`Welcome to the Gitpub App!`);
 });
 
+app.get('/drinks', (req,res)=>{
+    res.render('drinks_index.ejs', {
+        allDrinks: drinks,
+        allFoods: foods,
+    });
+});
+
+
+app.get('/drinks/:id', (req, res)=>{
+    res.render('drinks_show.ejs', {
+        drink: drinks[req.params.id],
+    });
+});
+
+app.get('/foods/:id', (req, res) => {
+    res.render('foods_show.ejs', {
+        food: foods[req.params.id],
+    });
+});
+
 //port
 app.listen(PORT, ()=>{
     console.log(`Listening in port`, PORT);
