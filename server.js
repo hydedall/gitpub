f5738f7957529b0714043852cb071db8eb1ba5cb
hydedall <hyde.dallas1@gmail.com>
2022-08-30 23:10:57 -0400

    roots port

--- server.js
+++ server.js
@@ -4,3 +4,13 @@ const foods = require('./models/food')
 
 const app = express();
 const PORT = 3000;
+
+//Routes
+app.get('/', (req, res)=>{
+    res.send(`Welcome to the Gitpub App!`);
+});
+
+//port
+app.listen(PORT, ()=>{
+    console.log(`Listening in port`, PORT);
+});
